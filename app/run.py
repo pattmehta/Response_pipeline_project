e253ad4f1d0fd77f38475db83b7498adeb23571d
Prateek Mehta <patt.mehta.official@gmail.com>
2023-01-29 12:38:11 -0800

    new plots, routes for new plots, templates, integration with master template using iframe

--- app/run.py
+++ app/run.py
@@ -42,6 +42,27 @@ model_path = f'../{model_path}'
 engine = create_engine(f'sqlite:///../{db_path}')
 df = pd.read_sql_table(tbl_name, engine)
 
+# data prep for plots start
+df_genre_vc = df['genre'].value_counts()
+df_genre_vc_x = list(df_genre_vc.index)
+df_genre_vc_y = list(df_genre_vc.values)
+
+df_category = df.loc[:,'aid_centers':'weather_related']
+df_category_topten = df_category.sum().sort_values(ascending=False)[:10]
+df_category_sort_x = list(df_category_topten.index)
+df_category_sort_y = list(df_category_topten.values)
+
+df_multicategory = df.drop(['message','original','id'],axis=1).groupby(by='genre',group_keys=True)
+selected_colnames = []
+def categories_agg(c):
+    if c.name in df_category_topten.index.values and c.name not in ['related','direct_report','other_aid','request']:
+        if c.name not in selected_colnames: selected_colnames.append(c.name)
+        return c.sum()
+    else: return False
+df_multicategory_agg = df_multicategory.aggregate(categories_agg)
+df_gc_x = list(df_multicategory_agg.index)
+# data prep for plots end
+
 # load model
 # model = joblib.load(model_path)
 model = None
@@ -51,6 +72,26 @@ if not model:
     print('could not load the model')
     exit()
 
+
+# routes start
+@app.route('/viz_genre')
+def viz_genre():
+    ids = [1]
+    graphJSON = create_genre_plot()
+    return render_template('viz_genre.html', ids=ids, graph=graphJSON)
+
+@app.route('/viz_category')
+def viz_category():
+    ids = [1]
+    graphJSON = create_category_plot()
+    return render_template('viz_category.html', ids=ids, graph=graphJSON)
+
+@app.route('/viz_multicategory')
+def viz_multicategory():
+    ids = [1]
+    graphJSON = create_multicategory_plot()
+    return render_template('viz_multicategory.html', ids=ids, graph=graphJSON)
+# routes end
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
@@ -114,6 +155,210 @@ def go():
 def main():
     app.run(host='0.0.0.0', port=3000, debug=True)
 
+
+# plots start
+def create_genre_plot():
+    graph = \
+        {
+            'data': [
+                Bar({
+                    'marker': {'color': '#1F77B4', 'line': {'width': 1.0}},
+                    'opacity': 1,
+                    'orientation': 'v',
+                    'x': df_genre_vc_x,
+                    'xaxis': 'x',
+                    'y': df_genre_vc_y,
+                    'yaxis': 'y',
+                })
+            ],
+            'layout': {
+                'title': 'Genre Plot',
+                'autosize': False,
+                'bargap': 0.5,
+                'height': 480,
+                'hovermode': 'closest',
+                'margin': {'b': 52, 'l': 80, 'pad': 0, 'r': 63, 't': 57},
+                'showlegend': False,
+                'template': '...',
+                'width': 640,
+                'xaxis': {'anchor': 'y',
+                        'showgrid': True,
+                        'showline': True,
+                        'side': 'bottom',
+                        'tickfont': {'size': 10.0},
+                        'ticks': '',
+                        'type': 'category',
+                        'category_orders': {'x':df_genre_vc_x},
+                        'zeroline': False},
+                'yaxis': {'anchor': 'x',
+                        'domain': [0.0, 1.0],
+                        'mirror': 'ticks',
+                        'showgrid': True,
+                        'showline': True,
+                        'side': 'left',
+                        'tickfont': {'size': 10.0},
+                        'ticks': 'inside',
+                        'type': 'linear',
+                        'zeroline': False}
+            }
+        }
+    
+    graphJSON = json.dumps(graph, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
+
+def create_category_plot():
+    graph = \
+        {
+            'data': [
+                Bar({
+                    'marker': {'color': '#1F77B4', 'line': {'width': 1.0}},
+                    'opacity': 1,
+                    'orientation': 'v',
+                    'x': df_category_sort_x,
+                    'xaxis': 'x',
+                    'y': df_category_sort_y,
+                    'yaxis': 'y',
+                })
+            ],
+            'layout': {
+                'title': 'Category Plot',
+                'autosize': False,
+                'bargap': 0.5,
+                'height': 480,
+                'hovermode': 'closest',
+                'margin': {'b': 52, 'l': 80, 'pad': 0, 'r': 63, 't': 57},
+                'showlegend': False,
+                'template': '...',
+                'width': 640,
+                'xaxis': {'anchor': 'y',
+                        'showgrid': True,
+                        'showline': True,
+                        'side': 'bottom',
+                        'tick0': 0,
+                        'tickfont': {'size': 10.0},
+                        'ticks': '',
+                        'type': 'category',
+                        'category_orders': {'x':df_category_sort_x},
+                        'zeroline': False},
+                'yaxis': {'anchor': 'x',
+                        'domain': [0.0, 1.0],
+                        'mirror': 'ticks',
+                        'showgrid': True,
+                        'showline': True,
+                        'side': 'left',
+                        'tickfont': {'size': 10.0},
+                        'ticks': 'inside',
+                        'type': 'linear',
+                        'zeroline': False}
+            }
+        }
+    
+    graphJSON = json.dumps(graph, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
+
+def create_multicategory_plot():
+    graph = \
+        {
+            'data': [
+                Bar({
+                    'opacity': 1,
+                    'orientation': 'v',
+                    'x': [-0.20833333333333331, 0.7916666666666667, 1.7916666666666665],
+                    'xaxis': 'x',
+                    'y': df_multicategory_agg[selected_colnames[0]],
+                    'yaxis': 'y',
+                    'name': selected_colnames[0]
+                }),
+                Bar({
+                    'opacity': 1,
+                    'orientation': 'v',
+                    'x': [-0.125, 0.875, 1.875],
+                    'xaxis': 'x',
+                    'y': df_multicategory_agg[selected_colnames[1]],
+                    'yaxis': 'y',
+                    'name': selected_colnames[1]
+                }),
+                Bar({
+                    'opacity': 1,
+                    'orientation': 'v',
+                    'x': [-0.04166666666666668, 0.9583333333333333, 1.958333333333333],
+                    'xaxis': 'x',
+                    'y': df_multicategory_agg[selected_colnames[2]],
+                    'yaxis': 'y',
+                    'name': selected_colnames[2]
+                }),
+                Bar({
+                    'opacity': 1,
+                    'orientation': 'v',
+                    'x': [0.04166666666666662, 1.0416666666666665, 2.0416666666666665],
+                    'xaxis': 'x',
+                    'y': df_multicategory_agg[selected_colnames[3]],
+                    'yaxis': 'y',
+                    'name': selected_colnames[3]
+                }),
+                Bar({
+                    'opacity': 1,
+                    'orientation': 'v',
+                    'x': [0.125, 1.125, 2.125],
+                    'xaxis': 'x',
+                    'y': df_multicategory_agg[selected_colnames[4]],
+                    'yaxis': 'y',
+                    'name': selected_colnames[4]
+                }),
+                Bar({
+                    'opacity': 1,
+                    'orientation': 'v',
+                    'x': [0.20833333333333331, 1.208333333333333, 2.208333333333334],
+                    'xaxis': 'x',
+                    'y': df_multicategory_agg[selected_colnames[5]],
+                    'yaxis': 'y',
+                    'name': selected_colnames[5]
+                })
+            ],
+            'layout': {
+                'title': 'Multicategory Plot',
+                'autosize': False,
+                'bargap': 0.0,
+                'height': 480,
+                'hovermode': 'closest',
+                'margin': {'b': 52, 'l': 80, 'pad': 0, 'r': 63, 't': 57},
+                'showlegend': True,
+                'template': '...',
+                'width': 640,
+                'xaxis': {'anchor': 'y',
+                        'domain': [0.0, 1.0],
+                        'dtick': 1,
+                        'mirror': 'ticks',
+                        'range': [-0.5, 2.5],
+                        'showgrid': True,
+                        'showline': True,
+                        'side': 'bottom',
+                        'tickfont': {'size': 10.0},
+                        'ticks': '',
+                        'tickmode': 'array',
+                        'ticktext': df_gc_x,
+                        'tickvals': list(range(0,len(df_gc_x))),
+                        'title': {'font': {'color': '#000000', 'size': 10.0}, 'text': 'genre'},
+                        'type': 'linear',
+                        'zeroline': False},
+                'yaxis': {'anchor': 'x',
+                        'domain': [0.0, 1.0],
+                        'mirror': 'ticks',
+                        'nticks': 8,
+                        'range': [0.0, 6103.65],
+                        'showgrid': True,
+                        'showline': True,
+                        'side': 'left',
+                        'tickfont': {'size': 10.0},
+                        'ticks': 'inside',
+                        'type': 'linear',
+                        'zeroline': False}
+            }
+        }
+
+    graphJSON = json.dumps(graph, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
+# plots end
 
 if __name__ == '__main__':
     main()
